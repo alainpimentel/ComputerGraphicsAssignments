02bd9e94fe499a666096cc56af2a5ad9c31943c8
hpimentel <hpimentel@southern.edu>
2014-09-08 19:52:21 -0400

    Some refactoring

--- 1 Chaotic Sierpinski Triangle/SierpinskiTriangle.cpp
+++ 1 Chaotic Sierpinski Triangle/SierpinskiTriangle.cpp
@@ -1,29 +1,33 @@
+//  Name: Alain Pimentel
+//  Assignment number: 1
+//  Assignment: Sierpinski Triangle
+//  File name: SierpinskiTriangle.cpp
+//  Date last modified: September 8, 2014
+//  Honor statement: I have neither given nor received any unauthorized help on this assignment. 
 #include <iostream>
 #include <math.h>
 #include <time.h>
 #include <GL/glut.h>
 
-/* QIUESTION 
- *
- * 1. Display dots when they are clicked
- * 2. 
- * TODO: drag a point to a new position
- */
-
 const int screenWidth = 1200;
 const int screenHeight = 900;
-int numberDots = 256; // COntrols how many dots to draw for the triangle
-static int numCorners = 0; // Keeps track of how many corners have been inputted
-bool dragging = false; // flag to control the draggin state
-int draggingPoint = -1; // -1 means no point is being dragged. Use 0 - 2 depending on which point is being dragged
+int numberDots = 256;		// COntrols how many dots to draw for the triangle
+static int numCorners = 0;	// Keeps track of how many corners have been inputted
+bool dragging = false;		// flag to control the draggin state
+int draggingPoint = -1;		// -1 means no point is being dragged. Use 0 - 2 depending on which point is being dragged
 
-// Holds a point
+// a coordinate structure
 struct GLintPoint{
 	GLint x, y;
 };
-GLintPoint corner[4];
+GLintPoint corner[4];		// Array to hold the triangle's points and seed
 
-// Draws the next dot on the screen
+/**
+Draws the next dot on the screen
+
+@param x x-coordinate
+@param y y-coordinate
+*/
 void drawDot(GLint x, GLint y) {
 	glBegin(GL_POINTS);
 	glVertex2i(x, y);
@@ -34,13 +38,16 @@ int random(int m) {
 	return rand() % m;
 }
 
-// Displays the Sierpinski Triangle
+/**
+Displays the Sierpinski Triangle
+
+@param corner1, corner2, corner 3 The three points to form a triangle
+@param seed A point to be used as a seed to generate the triangle
+*/
 void sierpinskiRender(GLintPoint corner1, GLintPoint corner2, GLintPoint corner3, GLintPoint seed) {
 	glClear(GL_COLOR_BUFFER_BIT); // clear the screen
-	//glClearColor(1.0, 1.0, 1.0, 0.0);
 	GLintPoint T[3] = { { corner1.x, corner1.y }, { corner2.x, corner2.y }, { corner3.x, corner3.y } }; // vertices of the triangle
-	//std::cout << corner1.x << "   " << corner1.y << "\n   " << corner2.x << "   " << corner2.y << "\n   " << 
-		//corner3.x << "   " << corner3.y << seed.x << "   " << seed.y << std::endl;
+
 	for (int i = 0; i < numberDots; i++) {
 		int index = random(3);
 		GLint x = (seed.x + T[index].x) / 2; // generate midpoint between randomly chosen vertice and seed
@@ -52,9 +59,8 @@ void sierpinskiRender(GLintPoint corner1, GLintPoint corner2, GLintPoint corner3
 }
 
 
-
+// Keyboard listener
 void myKeyboard(unsigned char ch, int, int) {
-	//std::cout << "You pressed " << ch << std::endl;
 	if (corner != NULL) {
 		if (ch == 'U') { // Double the number of dots up to 1048756
 			numberDots *= 2;
@@ -76,18 +82,17 @@ void myKeyboard(unsigned char ch, int, int) {
 	}
 }
 
+// Single mouse click listener
 void myMouse(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) { // Controls single clicks to draw the triangle
-		//std::cout << numCorners << std::endl;
 		corner[numCorners].x = x;
 		corner[numCorners].y = screenHeight - y;
 		if (++numCorners == 4) { // if four clicks were saved, draw triangle
 			sierpinskiRender(corner[0], corner[1], corner[2], corner[3]);
 		}
 	}
-	else if (dragging && button == GLUT_LEFT_BUTTON && state == GLUT_UP) { // Controls when the user lets go of the left click
-		// draws a new triangle if a point was moved
-		if (draggingPoint > -1 && numCorners > 4) { // Only drag when all points have been inputted
+	else if (dragging && button == GLUT_LEFT_BUTTON && state == GLUT_UP) { // Controls when the user lets go of the left click 
+		if (draggingPoint > -1 && numCorners > 4) {
 			dragging = false; // user can drag a new point
 			draggingPoint = -1;
 		}
@@ -134,13 +139,10 @@ void myMovedMouse(int x, int y) {
 			corner[3] = newPoint;
 
 		sierpinskiRender(corner[0], corner[1], corner[2], corner[3]); // redraw triangle with the new point
-
-		
 	}
-
 }
 
-// Initializes various values
+// Initializes the screen
 void myInit() {
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glColor3f(0.0f, 0.0f, 1.0f);
@@ -150,7 +152,7 @@ void myInit() {
 	gluOrtho2D(0.0, screenWidth, 0.0, screenHeight);
 }
 
-void stuff() {
+void displayFunc() {
 	glClear(GL_COLOR_BUFFER_BIT); // clear the screen
 	glFlush();
 }
@@ -165,7 +167,7 @@ int main(int argc, char** argv) {
 	// open the screen window (Title: my first attempt)
 	glutCreateWindow("Sierpinski Assignment");
 	// register the callback functions
-	glutDisplayFunc(stuff);
+	glutDisplayFunc(displayFunc);
 	glutMouseFunc(myMouse);
 	glutMotionFunc(myMovedMouse);
 	glutKeyboardFunc(myKeyboard);
